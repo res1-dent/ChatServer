45fbd120905c4bdafbc1d091c3ebbaba4c13185c
Андрей Александрович <res1.main@gmail.com>
2022-05-01 02:31:13 +0300

    deploy

--- src/main/kotlin/com/sometime/di/MainModule.kt
+++ src/main/kotlin/com/sometime/di/MainModule.kt
@@ -11,7 +11,7 @@ val mainModule = module {
     single {
         KMongo.createClient()
             .coroutine
-            .getDatabase("mongodb+srv://res1dent:<password>@cluster0.zr1iq.mongodb.net/Project 0?retryWrites=true&w=majority")
+            .getDatabase("mongodb+srv://res1dent:<password>@cluster0.zr1iq.mongodb.net/Cluster0?retryWrites=true&w=majority")
             //.getDatabase("message_db")
     }
     single<MessageDataSource> {
